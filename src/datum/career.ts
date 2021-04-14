84dd97a94e6b08d473db9eee78e1a2615d9fac75
eomttt <hyunt0413@gmail.com>
2021-04-14 12:58:48 +0900

    경력 및 프로젝트 추가

--- src/datum/career.ts
+++ src/datum/career.ts
@@ -1,44 +1,46 @@
 export const CareerItems = [
   {
-    image: 'https://media-exp1.licdn.com/dms/image/C560BAQFUdrs_XLi4YA/company-logo_100_100/0/1584683576466?e=1620259200&v=beta&t=oesBjAMMpLjjle98qZHFuYJgJoSZo_lA3ZWGBEvQSPk',
-    title: 'Ridi Corp',
+    image:
+      'https://media-exp1.licdn.com/dms/image/C560BAQEEljaHYBJimw/company-logo_100_100/0/1603692972619?e=1626307200&v=beta&t=-9X32e6IB6Tg51jpJ6q09acmQEx2fFGNhDoXAWm_ris',
+    title: 'Fitpet',
     startDate: '2020.03',
     finDate: 'now',
+    proejctList: ['NextJS 를 활용한 핏펫몰 리뉴얼'],
+    techStacks: ['ReactJS', 'NextJS', 'Emotion'],
+  },
+  {
+    image:
+      'https://media-exp1.licdn.com/dms/image/C560BAQFUdrs_XLi4YA/company-logo_100_100/0/1584683576466?e=1620259200&v=beta&t=oesBjAMMpLjjle98qZHFuYJgJoSZo_lA3ZWGBEvQSPk',
+    title: 'Ridi Corp',
+    startDate: '2020.03',
+    finDate: '2021.03',
     projectList: [
       'React Native 를 활용하여 Ridibooks App',
       'Electron 을 활용하여 Ridibooks PC Viewer',
     ],
-    techStacks: [
-      'ReactJS', 'React-Native', 'Electron', 'Emotion',
-    ],
+    techStacks: ['ReactJS', 'React-Native', 'Electron', 'Emotion'],
   },
   {
-    image: 'https://media-exp1.licdn.com/dms/image/C510BAQHL5BYKlMb52g/company-logo_100_100/0/1531470229879?e=1620259200&v=beta&t=33tfZs_JaYBvIzzuOHxlfeBDrQ9Sim0HohQZ-K5BG84',
+    image:
+      'https://media-exp1.licdn.com/dms/image/C510BAQHL5BYKlMb52g/company-logo_100_100/0/1531470229879?e=1620259200&v=beta&t=33tfZs_JaYBvIzzuOHxlfeBDrQ9Sim0HohQZ-K5BG84',
     title: 'WAUG Travel',
     startDate: '2019.08',
     finDate: '2020.03',
-    projectList: [
-      '다수의 Waug page 를 Next 를 활용하여 리뉴얼',
-    ],
-    techStacks: [
-      'ReactJS', 'NextJS', 'Styled-Component',
-    ],
+    projectList: ['다수의 Waug page 를 Next 를 활용하여 리뉴얼'],
+    techStacks: ['ReactJS', 'NextJS', 'Styled-Component', 'Jest'],
   },
   {
-    image: 'https://media-exp1.licdn.com/dms/image/C560BAQE6Tq6ZCZfxLg/company-logo_100_100/0/1581556526547?e=1620259200&v=beta&t=kYaXez2ONR6XFWD-kXjDtMjBNmKvYerujwr8ttlmYzs',
+    image:
+      'https://media-exp1.licdn.com/dms/image/C560BAQE6Tq6ZCZfxLg/company-logo_100_100/0/1581556526547?e=1620259200&v=beta&t=kYaXez2ONR6XFWD-kXjDtMjBNmKvYerujwr8ttlmYzs',
     title: 'SavannaBoat',
     startDate: '2017.05',
     finDate: '2019.05',
-    projectList: [
-      '밤편지 SNS 웹/앱/서버 개발',
-      '사바나보트 SNS 앱/서버 개발',
-    ],
-    techStacks: [
-      'ReactJS', 'Cordova/Ionoc', 'NodeJS', 'AngularJS', 'MongoDB',
-    ],
+    projectList: ['밤편지 SNS 웹/앱/서버 개발', '사바나보트 SNS 앱/서버 개발'],
+    techStacks: ['ReactJS', 'Cordova/Ionoc', 'NodeJS', 'AngularJS', 'MongoDB'],
   },
   {
-    image: 'https://media-exp1.licdn.com/dms/image/C510BAQFqpWitUc0A_g/company-logo_100_100/0/1557295147354?e=1620259200&v=beta&t=3GO8t_6DTpsI9LH4O2C5T3uUOMD6IRG93SpbsVfwZt8',
+    image:
+      'https://media-exp1.licdn.com/dms/image/C510BAQFqpWitUc0A_g/company-logo_100_100/0/1557295147354?e=1620259200&v=beta&t=3GO8t_6DTpsI9LH4O2C5T3uUOMD6IRG93SpbsVfwZt8',
     title: 'Naran',
     startDate: '2015.09',
     finDate: '2017.05',
@@ -47,8 +49,6 @@ export const CareerItems = [
       'Microbot Remote Firmware 개발',
       'Microbot App 개발',
     ],
-    techStacks: [
-      'AngularJS', 'Python', 'C++',
-    ],
+    techStacks: ['AngularJS', 'Python', 'C++'],
   },
 ];
--- src/datum/personal.ts
+++ src/datum/personal.ts
@@ -1,83 +1,62 @@
 export const PersonalItems = [
   {
+    title: 'React formatter',
+    projectList: ['Simple create react app'],
+    techStacks: ['JavaScript'],
+  },
+  {
+    title: 'React Native carousel',
+    projectList: ['React Native 용 simple carousel 제작'],
+    techStacks: ['React JS', 'React Native'],
+  },
+  {
+    title: 'Object key case converter',
+    projectList: ['JavaScript Object key case converter (camelCase <-> snake_case'],
+    techStacks: ['JavaScript'],
+  },
+  {
+    title: 'Poke docs',
+    projectList: ['Next JS, react-query 를 이용한 Poke docs'],
+    techStacks: ['Next JS', 'react-query'],
+  },
+  {
     title: 'React drag and select',
-    projectList: [
-      'React drag and select 모듈 제작',
-    ],
-    techStacks: [
-      'React JS',
-    ],
+    projectList: ['React drag and select 모듈 제작'],
+    techStacks: ['React JS'],
   },
   {
     title: 'React tree multi selector',
-    projectList: [
-      'React tree multi selector 모듈 제작',
-    ],
-    techStacks: [
-      'React JS',
-    ],
+    projectList: ['React tree multi selector 모듈 제작'],
+    techStacks: ['React JS'],
   },
   {
     title: 'ArtofJiujitsu auto player',
-    projectList: [
-      '주짓수 동영상 자동 플레이어',
-      'Heroku 를 사용하여 배포',
-    ],
-    techStacks: [
-      'Node JS',
-      'JavaScript',
-    ],
+    projectList: ['주짓수 동영상 자동 플레이어', 'Heroku 를 사용하여 배포'],
+    techStacks: ['Node JS', 'JavaScript'],
   },
   {
     title: 'KJ Glass',
-    projectList: [
-      '기존 카페 24 로 되어있는 페이지를 Next JS 를 활용하여 리뉴얼',
-    ],
-    techStacks: [
-      'Next JS',
-      'MobX',
-      'Styled-Component',
-    ],
+    projectList: ['기존 카페 24 로 되어있는 페이지를 Next JS 를 활용하여 리뉴얼'],
+    techStacks: ['Next JS', 'MobX', 'Styled-Component'],
   },
   {
     title: 'Dartour',
-    projectList: [
-      'Dart game controller',
-    ],
-    techStacks: [
-      'Angular JS',
-      'Ionic/Cordova',
-    ],
+    projectList: ['Dart game controller'],
+    techStacks: ['Angular JS', 'Ionic/Cordova'],
   },
   {
     title: '뉴스 삼삼삼',
-    projectList: [
-      '뉴스 레터 서빙 서비스',
-    ],
-    techStacks: [
-      'React-Native',
-      'Node JS',
-      'Python',
-    ],
+    projectList: ['뉴스 레터 서빙 서비스'],
+    techStacks: ['React-Native', 'Node JS', 'Python'],
   },
   {
     title: 'Keep roller',
-    projectList: [
-      '크로스핏 타이머',
-    ],
-    techStacks: [
-      'Angular JS',
-      'Ionic/Cordova',
-    ],
+    projectList: ['크로스핏 타이머'],
+    techStacks: ['Angular JS', 'Ionic/Cordova'],
   },
   {
     title: '오늘의 달',
-    projectList: [
-      '오늘의 달 모양 확인 및 사진찍기 기능 앱',
-    ],
-    techStacks: [
-      'React-Native',
-      'Node JS',
-    ],
+    projectList: ['오늘의 달 모양 확인 및 사진찍기 기능 앱'],
+    techStacks: ['React-Native', 'Node JS'],
   },
 ];
